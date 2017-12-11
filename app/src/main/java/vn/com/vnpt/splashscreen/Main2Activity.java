49383c169119d5d0edf13e890ca74b7f8bbec9db
Nhat <nhat.dinhminh@uni.dntu.edu.vn>
2017-12-11 14:43:48 +0700

    haha

---
+++ app/src/main/java/vn/com/vnpt/splashscreen/Main2Activity.java
@@ -0,0 +1,16 @@
+package vn.com.vnpt.splashscreen;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
